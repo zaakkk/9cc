24a7cadf4463a7ddf7447f132ea895eca4d88271
Taisei Yamazaki <17aj148@ms.dendai.ac.jp>
2019-09-16 23:35:22 +0900

    "掛け算追加"

--- codegen.c
+++ codegen.c
@@ -21,6 +21,11 @@ void gen_x86(Vector *irv){
 		case '-':
 			printf("	sub %s, %s\n", regs[ir->lhs], regs[ir->rhs]);
 			break;
+		case '*':
+			printf("	mov rax, %s\n", regs[ir->rhs]);
+			printf("	mul %s\n", regs[ir->lhs]);
+			printf("	mov %s, rax\n", regs[ir->lhs]);
+			break;
 		case IR_NOP:
 			break;
 		default:
@@ -29,66 +34,3 @@ void gen_x86(Vector *irv){
 	}
 }
 
-
-/*
-void gen(Node *node){
-	if(node->kind == ND_NUM){
-		printf("	push %d\n", node->val);
-		return;
-	}
-
-	gen(node->lhs);
-	gen(node->rhs);
-
-	switch(node->kind){
-		case ND_ADD:
-			printf("	add rax, rdi\n");
-			break;
-
-		case ND_SUB:
-			printf("	sub rax, rdi\n");
-			break;
-
-		case ND_MUL:
-			printf("	imul rax, rdi\n");
-			break;
-
-		case ND_DIV:
-			printf("	cqo\n");
-			printf("	idiv rdi\n");
-			break;
-		
-		case ND_EQ:
-			printf("	cmp rax, rdi\n");
-			printf("	sete al\n");
-			printf("	movzb rax, al\n");
-			break;
-
-		case ND_NE:
-			printf("	cmp rax, rdi\n");
-			printf("	setne al\n");
-			printf("	movzb rax, al\n");
-			break;
-
-		case ND_LT:
-			printf("	cmp rax, rdi\n");
-			printf("	setl al\n");
-			printf("	movzb rax, al\n");
-			break;
-
-		case ND_LE:
-			printf("	cmp rax, rdi\n");
-			printf("	setle al\n");
-			printf("	movzb rax, al\n");
-			break;
-	}
-
-	printf("	pop rdi\n");
-	printf("	pop rax\n");
-
-	
-
-	printf("	push rax\n");
-}
-
-*/
